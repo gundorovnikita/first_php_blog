dd63d686c0f599a0995141146aefcac4a2b22a1a
harai <harai@harai.harai>
2019-07-24 02:41:33 +0400

    1

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,7 +5,7 @@
 		@if($post->image != 'get')
 			<img src="{{url('images/'.$post->image)}}" width="200px">
 		@else
-			<img src="{{url('media/1.jpg')}}" alt="" width="200px">
+			<img src="{{url('media/1.svg.png')}}" alt="" width="200px">
 		@endif
 		<div class="article">{{ $post->title }}</div> 
 		<div class="article">{{ $post->category->name }}</div>
